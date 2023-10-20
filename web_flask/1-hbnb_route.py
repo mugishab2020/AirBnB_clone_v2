e98ce391d93af79b46129a5588b1865ddf0c8b41
mugishab2020 <mugishab2020@gmail.com>
2023-10-20 05:53:52 -0700

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,11 +7,17 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Hello_hbnb function routing and return Hello_hbnb
+    """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """
+    hbnb routing and returning HBNB
+    """
     return 'HBNB'
 
 if __name__ == '__main__':
